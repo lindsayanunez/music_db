82602f1a37173ac2e09bb7cb98c0ae3858fc6953
Lindsay Nunez <lindsayanunez@gmail.com>
2015-11-09 09:29:15 -0600

    added artist to genre show view and added songs to all artists + added annotate gem.

---
+++ test/models/artist_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class ArtistTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
+
+# == Schema Information
+#
+# Table name: artists
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  genre_id   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_artists_on_genre_id  (genre_id)
+#
---
+++ test/models/song_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class SongTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
+
+# == Schema Information
+#
+# Table name: songs
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  artist_id  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_songs_on_artist_id  (artist_id)
+#
